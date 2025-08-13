885b554819cce2ece388fcd8635381f74a6e257f
Luiza, Rafaella e Sarah <2Dlusarra@gmail.com>
2025-08-13 08:55:33 -0300

    Aula 13 ago.

--- perguntas.js
+++ perguntas.js
@@ -5,17 +5,17 @@ criaCartao(
 )
 
 criaCartao(
-    ‘Programação’,
-    "O que é o Javascript",
-    ‘O JavaScript é uma linguagem de programação’
+    "Programação";
+    "O que é o Javascript";
+    "O JavaScript é uma linguagem de programação";
 )
 criaCartao(
-    ‘Geografia’,
-    ‘Qual a capital da França?’,
-‘A capital da França é Paris’
+    "Geografia";
+    "Qual a capital da França?";"
+    "A capital da França é Paris";
 )
 criaCartao(
-‘Programação’,
-‘O que é uma função?’,
-‘Uma função é um bloco de código que executa alguma tarefa’
+    "Programação";
+    "O que é uma função?";
+    "Uma função é um bloco de código que executa alguma tarefa";
 )
